800bfb763520926cee6bed4686e429cc5141b692
jugrajn <jugraj.nijjar@gmail.com>
2021-07-11 13:57:25 -0400

    updated server: was missing middleware and session

--- SERVER.js
+++ SERVER.js
@@ -1,19 +1,32 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session =require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
-const helpers = require('./utils/hehlpers');
+const helpers = require('./utils/helpers');
 const sequelize = require('./config/connecton');
 
 const app = express();
 const PORT = process.env.PORT || 3002;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
