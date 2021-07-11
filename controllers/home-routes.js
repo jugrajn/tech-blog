3d95bba49d01d26c11674a93ef2edf3bfe764ff5
jugrajn <jugraj.nijjar@gmail.com>
2021-07-11 13:45:27 -0400

    created route for dashboard page, fixed typo for homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
         const posts = postData.map((post) => post.get({ plain: true }))
         res.render('homepage', {
             posts,
-            loggedIn: req.session.logginIn,
+            loggedIn: req.session.loggedIn,
         });
     } 
     
@@ -24,18 +24,29 @@ router.get('/', async (req, res) => {
 //CREATE ROUTE FOR DASHBOARD PAGE
 router.get('/dashboard', async (req, res) => {
     try {
+        const dashData = await Post.findAll({
+            where: { user_id: req.session.user_id},
+            include: [{ model: User }]
+        });
 
+        const posts = dashData.map((post) => post.get({ plain: true }));
+
+        // NOW USE THE MAPPED DATA and RENDER ON DASHBOARD PAGE
+        res.render('dashboard', {
+            posts,
+            loggedIn: req.session.loggedIn
+        });
     }
 
     catch (err) {
-
+        res.status(400).json({ message: 'Please login or signup.'})
     }
 });
 
 //CREATE ROUTE FOR LOGIN
 router.get('/login', async (req, res) => {
     if (req.session.loggedIn) {
-        res.redirect('/');
+        res.redirect('/homepage');
         return;
     }
     res.render('login');
