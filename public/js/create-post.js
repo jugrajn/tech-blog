7a6a715679587dd5bb0174e23d251cb8e1e5112d
jugrajn <jugraj.nijjar@gmail.com>
2021-07-17 16:26:17 -0400

    added add post event listener function client side code

---
+++ public/js/create-post.js
@@ -0,0 +1,20 @@
+// EVENT LISTENER FUNCTION for ADDING POST
+
+const addPostFormHandler = async event => {
+    event.preventDefault();
+    const postTitle = document.querySelector('#post-title').value.trim();
+    const postContent = document.querySelector('#post-content').value.trim();
+
+    const postRes = await fetch('/api/post', {
+        method: 'POST',
+        body: JSON.stringify({ postTitle, postContent }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (postRes.ok) {
+        document.location.replace('/dashboard');
+    }
+};
+
+// Call Event Listenor function
+document.querySelector('.add-button').addEventListener('submit', addPostFormHandler);
