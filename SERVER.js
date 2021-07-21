cd410d0878bf4b2b3c7c71b69f3db19bd05720e0
jugrajn <jugraj.nijjar@gmail.com>
2021-07-20 20:16:07 -0400

    updated server for heroku

--- SERVER.js
+++ SERVER.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3002;
 
 const sess = {
-  secret: 'process.env.SECRET || Super secret secret',
+  secret: 'process.env.SECRET',
   cookie: {},
   resave: false,
   saveUninitialized: true,
